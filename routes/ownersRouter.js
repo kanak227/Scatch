ab60a4d446cfa91303452e34d8b031e74eac0337
kanak227 <kanakverma325@gmail.com>
2025-06-30 00:15:09 +0530

    setup cofig , debug and development condition for owner route

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model')
+
+if(process.env.NODE_ENV === "development"){
+    router.post('/create' , async function(req,res){
+        let owners = await ownerModel.find();
+        if(owners.length>0){
+            res
+            .status(503)
+            .send("You do not have service to create user");
+        }
+        let {fullname , email , password} = req.body;
+        let createdOwner = await ownerModel.create({
+            fullname ,
+            email ,
+            password,
+        })
+
+        res.send(createdOwner);
+    })
+}
 
 router.get('/' , function(req,res){
     res.send("working");
 })
+
+
 
 module.exports = router;
